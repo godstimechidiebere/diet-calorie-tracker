640d65592cc244a3d592c1c5c3566b65f05cfda7
godstimechidiebere <nwabufogodstime@gmail.com>
2024-05-02 16:40:27 +0100

    This is the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
-import 'package:dietonyi/screens/onboarding/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:dietonyi/app.dart';
 
 import 'package:dietonyi/screens/getting_started_screens.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: OnboardingScreen() //GettingStartedScreen(),
-        );
-  }
 }
