130ebb2a58248ad25e06262e2fe0e2b6f0a3d49f
godstimechidiebere <nwabufogodstime@gmail.com>
2024-05-02 18:56:57 +0100

    Refactor main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dietonyi/app.dart';
 
-import 'package:dietonyi/screens/getting_started_screens.dart';
-
 void main() {
   runApp(MyApp());
 }
