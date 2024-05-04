27fb5a7273589114492f2d93814c4bd011b0f08d
godstimechidiebere <nwabufogodstime@gmail.com>
2024-05-04 12:51:29 +0100

    refactor: Update screenSize and screenHeight functions
    This commit updates the `screenSize` and `screenHeight` functions in the `THelperFunctions` class. The functions now use the `Get.context` property instead of the `BuildContext` parameter, making them more flexible and easier to use. This change improves the readability and maintainability of the code.

--- lib/src/helpers/helper_functions.dart
+++ lib/src/helpers/helper_functions.dart
@@ -75,16 +75,16 @@ class THelperFunctions {
     return Theme.of(context).brightness == Brightness.dark;
   }
 
-  static Size screenSize(BuildContext context) {
-    return MediaQuery.of(context).size;
+  static Size screenSize() {
+    return MediaQuery.of(Get.context!).size;
   }
 
-  static double screenHeight(BuildContext context) {
-    return screenSize(context).height;
+  static double screenHeight() {
+    return MediaQuery.of(Get.context!).size.height;
   }
 
-  static double screenWidth(BuildContext context) {
-    return screenSize(context).width;
+  static double screenWidth() {
+    return MediaQuery.of(Get.context!).size.width;
   }
 
   //static String getFormattedDate(DateTime date, {String format = 'dd/MM/yyyy'}) {
