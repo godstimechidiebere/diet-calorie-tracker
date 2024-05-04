c611d5a4c34e3f5377c64e8e647590ee531dc3e0
godstimechidiebere <nwabufogodstime@gmail.com>
2024-05-04 12:48:19 +0100

    feat: Update onboarding controller to navigate to signup screen
    The `OnBoardingController` is updated to navigate to the `SignupScreen` when the user clicks on the "Skip" button. This allows users to skip the onboarding process and go directly to the signup screen.

--- lib/model/onboarding_controller.dart
+++ lib/model/onboarding_controller.dart
@@ -1,4 +1,5 @@
 import 'package:dietonyi/screens/login/login.dart';
+import 'package:dietonyi/screens/signup/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -21,7 +22,7 @@ class OnBoardingController extends GetxController {
   ///Update Current Index & jump to next page
   void nextPage() {
     if (currentPageIndex.value == 2) {
-      Get.offAll(LoginScreen());
+      Get.offAll(const LoginScreen());
     } else {
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);
@@ -32,5 +33,6 @@ class OnBoardingController extends GetxController {
   void skipPage() {
     currentPageIndex.value = 2;
     pageController.jumpToPage(2);
+    //Get.to(const LoginScreen());
   }
 }
