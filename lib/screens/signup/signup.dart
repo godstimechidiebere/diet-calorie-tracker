8d9c3b595bbfc1b3a77422c589e29f65ec250cd2
godstimechidiebere <nwabufogodstime@gmail.com>
2024-05-04 12:47:20 +0100

    chore: Update signup screen UI and dependencies

--- lib/screens/signup/signup.dart
+++ lib/screens/signup/signup.dart
@@ -1,7 +1,10 @@
+import 'package:dietonyi/screens/signup/verify_email.dart';
 import 'package:dietonyi/src/constants/sizes.dart';
 import 'package:dietonyi/src/constants/text_strings.dart';
 import 'package:dietonyi/widgets/login_signup/form_divider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
 import 'package:get/get.dart';
@@ -11,65 +14,69 @@ class SignupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Signup'),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(128.0),
-          child: Column(
-            children: [
-              ///Title
-              Text(tSignUpTitle1,
-                  style: Theme.of(context).textTheme.headlineMedium),
-              Text(tSignUpTitle2,
-                  style: Theme.of(context).textTheme.headlineMedium),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Signup'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(TSizes.spaceBtwSections * 3),
+            child: Column(
+              children: [
+                ///Title
+                Text(tSignUpTitle1,
+                    style: Theme.of(context).textTheme.headlineSmall),
+                Text(tSignUpTitle2,
+                    style: Theme.of(context).textTheme.headlineSmall),
 
-              const SizedBox(height: TSizes.spaceBtwSections),
+                const SizedBox(height: TSizes.spaceBtwSections),
 
-              /// Continue Button
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(tContinue),
+                /// Continue Button
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Get.to(() => VerifyEmailScreen());
+                    },
+                    child: const Text(tContinue),
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: TSizes.spaceBtwSections),
+                const SizedBox(height: TSizes.spaceBtwSections),
 
-              ///Divider
-              TFormDivider(dividerText: tOrSignUpWith.capitalize!),
-              const SizedBox(height: TSizes.spaceBtwSections),
+                ///Divider
+                TFormDivider(dividerText: tOrSignUpWith.capitalize!),
+                const SizedBox(height: TSizes.spaceBtwSections),
 
-              ///Social Buttons
+                ///Social Buttons
 
-              SizedBox(
-                width: double.infinity,
-                child: SignInButton(
-                  Buttons.Apple,
-                  text: "Sign up with Google",
-                  onPressed: () {},
+                SizedBox(
+                  width: double.infinity,
+                  child: SignInButton(
+                    Buttons.Google,
+                    text: "Sign up with Google",
+                    onPressed: () {},
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: TSizes.xs),
+                const SizedBox(height: TSizes.xs),
 
-              SizedBox(
-                width: double.infinity,
-                child: SignInButton(
-                  Buttons.Facebook,
-                  text: "Sign up with Facebook",
-                  onPressed: () {},
+                SizedBox(
+                  width: double.infinity,
+                  child: SignInButton(
+                    Buttons.Facebook,
+                    text: "Sign up with Facebook",
+                    onPressed: () {},
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: TSizes.xs),
+                const SizedBox(height: TSizes.xs),
 
-              //Footer Text
-              const Text(tPost),
-            ],
+                //Footer Text
+                const Text(tPost),
+              ],
+            ),
           ),
         ),
       ),
